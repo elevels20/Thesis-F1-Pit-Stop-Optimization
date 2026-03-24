6ac0cfd0fd33c9034d7f237a14832c121431057b
i6368803 <e.levels@student.maastrichtuniversity.nl>
2026-03-24 19:28:55 +0100

    Added stochastic model

---
+++ stochasticModel.py
@@ -0,0 +1,381 @@
+import numpy as np
+from scipy.optimize import linprog
+import math
+
+# PARAMETERS
+N = 52 # Number of laps 
+T = [1, 2, 3] # Tire compounds. 1 = Soft, 2 = Medium, 3 = Hard
+T0 = [0] + T # 0 = no pit stop
+
+u1 = 30 # Lifespan in number of laps for Soft tires
+u2 = 40 # Lifespan in number of laps for Medium tires
+u3 = 50 # Lifespan in number of laps for Hard tires
+u = [u1, u2, u3]
+
+d0 = {"A":97.22, "B":97.24} # Lap time for driver A/B when using new soft compound tires at the beginning of the race (without pit stops, interactions, or DRS)
+p0 = {"A":20.2, "B":20.0} # Additional lap time for driver A/B due to a pit stop
+
+lambda_pen = 2.0
+h = 0.02 # Lap time reduction between two consecutive laps attributed to fuel consumption (making the car lighter)
+
+d_VSC = 170 # Lap time during a VSC (without pit stop)
+d_SC = 200 # Lap time during a SC (without pit stop)
+
+p_VSC = {"A":10.0, "B":10.0} # Additional lap time for driver A/B due to a pit stop under VSC
+p_SC  = {"A":10.0, "B":10.0} #additional lap time for driver A/B due to a pit stop under SC
+
+g1 = -0.4
+
+# gap discretization
+g_min = -35.0
+g_max = 35.0
+g_step = 0.04
+g_values = np.arange(g_min, g_max + g_step, g_step)
+
+# Stochastic yellow flag 
+l_VSC = 2 # Laps that last a VSC
+l_SC = 3 # Laps that last a SC
+
+# Probability of having a VSC during a race is 0.3
+# probability of having a SC during a race is 0.47
+# q_VSC = 0.3 / N # Probability of having a VSC during a lab
+# q_SC = 0.47 / N # Probability of having a VSC during a lab
+
+r_VSC = 0.3 # Probability of having at least one VSC during the race
+r_SC = 0.3 # Probability of having at least one SC during the race
+
+# r = 1 - (1 - q) ** N
+# q = 1 - (1 - r) ** (1/N)
+q_VSC = 1 - (1 - r_VSC) ** (1/N)
+q_SC = 1 - (1 - r_SC) ** (1/N)
+
+k_VSC = 2 # Number of laps needed to be raced after the end of a VSC to enable the DRS
+k_SC = 2 # Number of laps needed to be raced after the end of a SC to enable the DRS
+
+# RANDOM VARIABLES
+Z1_vals = [0.2, 0.4, 0.6] 
+Z2_vals = [0.5, 0.7, 0.9]
+Z_prob = [1/3, 1/3, 1/3]
+
+TDRS_vals = [0.1, 0.3, 0.5]
+TDRS_prob = [0.2, 0.7, 0.1]
+
+# state = (tire_A, wA, mA, tire_B, wB, mB, g, y_VSC, y_SC, y_DRS)
+
+# FUNCTIONS
+
+# Tire-wear function
+def tire_wear(tire, w):
+    if tire == 1:
+        return 0.004 * (w - 1) + 0.005 * ((w - 1) ** 2)
+    elif tire == 2:
+        return 0.4 + 0.20 * w + 0.008 * (w ** 2)
+    elif tire == 3:
+        return 0.9 + 0.10 * w + 0.0001 * (w ** 2)
+
+# Interaction function
+def interaction_A(eps, Z1, Z2):
+    if eps < 0:
+        return np.exp(-lambda_pen * abs(eps)) * Z1
+    else:
+        return np.exp(-lambda_pen * abs(eps)) * Z2
+
+def interaction_B(eps, Z1, Z2):
+    return interaction_A(-eps, Z1, Z2)
+
+# Lap time functions
+def lap_time_VSC(driver, pit):    
+    return d_VSC + (p_VSC[driver] if pit else 0)
+
+def lap_time_SC(g, driver, pitA, pitB):
+    IA = 1 if pitA else 0
+    IB = 1 if pitB else 0
+
+    xi = 1 if (g + p_SC["A"] * IA - p_SC["B"] * IB) < 0 else 0
+
+    if driver == "A":
+        if xi == 1:
+            lap_time = d_SC + p_SC["A"] * IA
+        else:
+            lap_time = d_SC + p_SC["B"] * IA - g + 0.5
+    else:
+        if xi == 1:
+            lap_time = d_SC + p_SC["A"] * IB + g + 0.5
+        else:
+            lap_time = d_SC + p_SC["B"] * IB
+
+    return lap_time
+
+def g_next_under_SC(g, pitA, pitB):
+    IA = 1 if pitA else 0
+    IB = 1 if pitB else 0
+
+    xi = 1 if (g + p_SC["A"] * IA - p_SC["B"] * IB) < 0 else 0
+
+    return 0.5 * (-1) ** xi
+
+def lap_time_no_yellow_flag(driver, n, tire, w, pitA, pitB, g, y_DRS, Z1, Z2, T_DRS):
+    IA = 1 if pitA else 0
+    IB = 1 if pitB else 0
+
+    drs_A = 1 if (0 <= g <= 1 and y_DRS == 0) else 0
+    drs_B = 1 if (-1 <= g <= 0 and y_DRS == 0) else 0
+
+    eps = g + p0["A"] * IA - T_DRS * drs_A - (p0["B"] * IB - T_DRS * drs_B)
+
+    if driver=="A":
+        eta = interaction_A(eps,Z1,Z2)
+        pit = pitA
+        drs = drs_A
+    else:
+        eta = interaction_B(eps,Z1,Z2)
+        pit = pitB
+        drs = drs_B
+
+    gamma = d0[driver] + (p0[driver] if pit else 0) + tire_wear(tire, w) - h * n
+    
+    return gamma + eta - T_DRS * drs
+
+def final_lap_time(y_VSC, y_SC, driver, n, tire, w, pitA, pitB, g, y_DRS, Z1, Z2, T_DRS):
+    if y_VSC + y_SC == 0:
+        return lap_time_no_yellow_flag(driver, n, tire, w, pitA, pitB, g, y_DRS, Z1, Z2, T_DRS)
+    elif y_VSC > 0:
+        if driver == "A":
+            pit = pitA
+        else:
+            pit = pitB
+        return lap_time_VSC(driver, pit)
+    elif y_SC > 0:
+        return lap_time_SC(g, driver, pitA, pitB)
+    
+# State functions
+def t_next(tire, decision):
+    if decision == 0:
+        return tire
+    else:
+        return decision
+    
+def w_next(w, decision, Y_VSC, Y_SC, u):
+    I_driver = 1 if decision == 0 else 0
+    I_Y = 1 if Y_VSC + Y_SC == 0 else 0
+
+    return min(w * I_driver + I_Y, u + 1)
+
+def m_next(m, decision, tire):
+    if decision != 0 and decision != tire:
+        return max(m, 1)
+    else:
+        return max(m, 0)
+    
+def g_next(y_VSC, y_SC, n, tire_A, tire_B, wA, wB, pitA, pitB, g, y_DRS, Z1, Z2, T_DRS):
+    return g + final_lap_time(y_VSC, y_SC, "A", n, tire_A, wA, pitA, pitB, g, y_DRS, Z1, Z2, T_DRS) - final_lap_time(y_VSC, y_SC, "B", n, tire_B, wB, pitA, pitB, g, y_DRS, Z1, Z2, T_DRS)
+    
+def discretize_gap(g):
+    return min(g_values, key=lambda x: abs(x - g))
+
+def y_VSC_next(Y_VSC):
+    return Y_VSC
+
+def y_SC_next(Y_SC):
+    return Y_SC
+
+def y_DRS_next(y_DRS, Y_VSC, Y_SC):
+    I_Y = 1 if Y_VSC + Y_SC == 0 else 0
+    I_Y_VSC = 1 if Y_VSC > 0 else 0
+    I_Y_SC = 1 if Y_SC > 0 else 0
+
+    return max(y_DRS - 1, 0) * I_Y + (Y_VSC + k_VSC) * I_Y_VSC + (Y_SC + k_SC) * I_Y_SC
+
+def yellow_transitions(y_VSC, y_SC):
+    if y_VSC > 0:
+        return [((y_VSC - 1, 0), 1.0)]
+    if y_SC > 0:
+        return [((0, y_SC - 1), 1.0)]
+
+    return [
+        ((l_VSC, 0), q_VSC),
+        ((0, l_SC), q_SC),
+        ((0, 0), 1 - q_VSC - q_SC)
+    ]
+
+def state_next(tire_A, wA, mA, tire_B, wB, mB, g, y_VSC, y_SC, y_VSC_n, y_SC_n, y_DRS, n, decisionA, decisionB, z1, z2, t_DRS):
+    tire_A_n = t_next(tire_A, decisionA)
+    tire_B_n = t_next(tire_B, decisionB)
+
+    wA_n = w_next(wA, decisionA, y_VSC, y_SC, u[tire_A - 1])
+    wB_n = w_next(wB, decisionB, y_VSC, y_SC, u[tire_B - 1])
+
+    mA_n = m_next(mA, decisionA, tire_A)
+    mB_n = m_next(mB, decisionB, tire_B)
+
+    pitA = True if decisionA != 0 else False
+    pitB = True if decisionB != 0 else False
+
+    g_n = g_next(y_VSC, y_SC, n, tire_A, tire_B, wA, wB, pitA, pitB, g, y_DRS, z1, z2, t_DRS)
+    g_n = discretize_gap(g_n)
+
+    y_DRS_n = y_DRS_next(y_DRS, y_VSC, y_SC)
+
+    return (tire_A_n, wA_n, mA_n, tire_B_n, wB_n, mB_n, g_n, y_VSC_n, y_SC_n, y_DRS_n)
+
+# Stochastic dynamic programming
+def H(w, u, m): 
+    """
+    Returns whether the driver finishes the race adequately without using tires 
+    longer than their lifespans and using at least two different tire compounds.
+    """
+    return w <= u and m == 1
+
+def V_end(tire_A, wA, mA, tire_B, wB, mB, g):
+    Ha = H(wA, u[tire_A - 1], mA)
+    Hb = H(wB, u[tire_B - 1], mB)
+
+    if Ha:
+        if Hb:
+            return g
+        else:
+            return - math.inf
+    elif Hb:
+        return math.inf
+    else:
+        return 0
+
+# state = (tire_A, wA, mA, tire_B, wB, mB, g, y_VSC, y_SC, y_DRS)
+def generate_states(n):
+    states = []
+    for tire_A in T:
+        for tire_B in T:
+            for wA in range(u[tire_A - 1] + 2):
+                for wB in range(u[tire_B - 1] + 2):
+                    for mA in range(2):
+                        for mB in range(2):
+                            for g in g_values:
+                                for y_VSC in range(l_VSC + 1):
+                                    for y_SC in range(l_SC + 1):
+                                        for y_DRS in range(N - n + 1):
+                                            states.append((tire_A, wA, mA, tire_B, wB, mB, g, y_VSC, y_SC, y_DRS))
+    return states
+
+# Stochastic Dynamic Programming Algorithm
+def solve_SDP():
+    # Step 3: Compute 𝑉 ′^{*} _N+1(s_n) for all s_n ∈ S_N+1
+    states_final = generate_states(N + 1) # S_n+1
+    V = {state: 0 for state in states_final}
+    for state in states_final:
+        tire_A, wA, mA, tire_B, wB, mB, g, _, _, _ = state
+        V[state] = V_end(tire_A, wA, mA, tire_B, wB, mB, g)
+
+    # Policies
+    xA_star = {}
+    xB_star = {}
+
+    for n in range(N, 0, -1):
+        V_new = {}
+        states = generate_states(n)
+        for state in states:
+            tire_A, wA, mA, tire_B, wB, mB, g, y_VSC, y_SC, y_DRS = state
+
+            # Step 6: compute V'_n(s,a,b) for all a, b ∈ T0.
+            V_prime = {}
+            for a in T0:
+                for b in T0:
+                    val = 0
+
+                    for t_DRS, p_DRS in zip(TDRS_vals, TDRS_prob):
+                        for z1, p_z1 in zip(Z1_vals, Z_prob):
+                            for z2, p_z2 in zip(Z2_vals, Z_prob):
+                                for (y_VSC_n, y_SC_n), p_y in yellow_transitions(y_VSC, y_SC):
+                                    state_n = state_next(tire_A, wA, mA, tire_B, wB, mB, g, y_VSC, y_SC, y_VSC_n, y_SC_n, y_DRS, n, a, b, z1, z2, t_DRS)
+
+                                    prob = p_DRS * p_z1 * p_z2 * p_y
+                                    val += prob * V[state_n]                                    
+
+                    V_prime[(a, b)] = val
+
+            if g < 0: # A is leader (minimize g)
+                # Step 8: compute x_n^{B*}(s,a) for all a ∈ T0.
+                b_star = {a: max(T0, key=lambda b: V_prime[(a, b)]) for a in T0}
+
+                # Step 9: compute x_n^{A*}(s)
+                a_star = min(T0, key=lambda a: V_prime[(a, b_star[a])])
+
+                # Step 10: value update
+                V_new[state] = V_prime[(a_star, b_star[a_star])]
+
+                xA_star[(n, state)] = a_star
+                xB_star[(n, state)] = b_star[a_star]
+            
+            else: # B is leader (maximize g)
+                # Step 12: compute x_n^{A*}(s,b) for all b ∈ T0.
+                a_star = {b: min(T0, key=lambda a: V_prime[(a, b)]) for b in T0}
+
+                # Step 13: compute x_n^{B*}(s)
+                b_star = max(T0, key=lambda b: V_prime[(a_star[b], b)])
+
+                # Step 14: value update
+                V_new[state] = V_prime[(a_star[b_star], b_star)]
+
+                xA_star[(n, state)] = a_star[b_star]
+                xB_star[(n, state)] = b_star
+    
+        V = V_new
+    # return V, xA_star, xB_star
+
+    T_list = list(T0)
+    nT = len(T_list)
+
+    # Step 15: build payoff matrix U'
+    U = np.zeros((nT, nT))
+
+    for i, tA in enumerate(T_list):
+        for j, tB in enumerate(T_list):
+            s1 = (tA, 0, 0, tB, 0, 0, g1, 0, 0, 2)
+            U[i, j] = V[s1]
+
+    # Step 16: solve zero-sum game via LP   
+    # Player B (E.3)
+
+    # Variables: [π_B (nT), ρ]
+    c = np.zeros(nT + 1) # Coefficients of the objective function to minimize.
+    c[-1] = -1  # maximize ρ
+
+    A_ub = [] # Coefficients for the inequality constraints 
+    b_ub = [] # Right-hand side values for the inequality constraints.
+
+    # ρ - (U π_B)_i ≤ 0  →  -U[i,:] π_B + ρ ≤ 0
+    for i in range(nT):
+        A_ub.append([-U[i, j] for j in range(nT)] + [1])
+        b_ub.append(0)
+
+    A_eq = [[1]*nT + [0]] # Coefficients for the equality constraints (Ax = b).
+    b_eq = [1] # Right-hand side value for the equality constraints.
+
+    bounds = [(0,1)]*nT + [(None, None)] # Specifies the bounds for each variable in the form of a sequence of tuples.
+
+    resB = linprog(c, A_ub=A_ub, b_ub=b_ub, A_eq=A_eq, b_eq=b_eq, bounds=bounds, method='highs')
+
+    pi_B = resB.x[:-1]
+
+    # Player A (E.4)
+    # Variables: [π_A (nT), ρ]
+    c = np.zeros(nT + 1)
+    c[-1] = 1  # minimize ρ
+
+    A_ub = []
+    b_ub = []
+
+    # ρ - (U^T π_A)_i ≥ 0  →  (U^T π_A)_i - ρ ≤ 0
+    for i in range(nT):
+        A_ub.append([U[j, i] for j in range(nT)] + [-1])
+        b_ub.append(0)
+
+    A_eq = [[1]*nT + [0]]
+    b_eq = [1]
+
+    bounds = [(0,1)]*nT + [(None, None)]
+
+    resA = linprog(c, A_ub=A_ub, b_ub=b_ub, A_eq=A_eq, b_eq=b_eq, bounds=bounds, method='highs')
+
+    pi_A = resA.x[:-1]
+
+    return U, pi_A, pi_B
